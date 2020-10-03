7157b71eef070beab582fcf37c96c4a0e9f63de7
Quinn Larson <quinn@ramblehousegames.com>
2020-10-03 13:08:07 +0900

    Character Created, Rigged, and started Animating

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static GameStateManager Inst;
+
+    private GameState currentState = new IntroGameState();
+
+    void Awake()
+    {
+        Inst = this;
+    }
+
+    void Start()
+    {
+        currentState.Start();
+    }
+
+    void Update()
+    {
+        GameState nextState = currentState.Update();
+        if(nextState != currentState)
+        {
+            currentState.Finish();
+            currentState = nextState;
+            nextState.Start();
+        }
+    }
+}
+
+public abstract class GameState
+{
+    public virtual void Start() {}
+    public abstract GameState Update();
+    public virtual void Finish() {}
+}
+
+public class IntroGameState : GameState
+{
+    public override GameState Update()
+    {
+        return this;
+    }
+}
---
+++ Assets/Scripts/SignalManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SignalManager : MonoBehaviour
+{
+    public static SignalManager Inst;
+
+    private Dictionary<Type, Action<Signal>> listeners = new Dictionary<Type, Action<Signal>>();
+
+    void Awake()
+    {
+        Inst = this;
+    }
+
+    public void AddListener<T>(Action<Signal> callback) where T : Signal
+    {
+        if(listeners.ContainsKey(typeof(T)))
+            listeners[typeof(T)] += callback;
+        else
+            listeners.Add(typeof(T), callback);
+    }
+
+    public void RemoveListener<T>(Action<Signal> callback) where T : Signal
+    {
+        listeners[typeof(T)] -= callback;
+        if(listeners[typeof(T)] == null)
+            listeners.Remove(typeof(T));
+    }
+
+    public void FireSignal(Signal signal)
+    {
+        if(listeners.ContainsKey(signal.GetType()))
+            listeners[signal.GetType()](signal);
+    }
+}
+
+public abstract class Signal {}
+public class SignalTest : Signal 
+{
+    public string TestString;
+
+    public SignalTest(string testString)
+    {
+        this.TestString = testString;
+    }
+}
